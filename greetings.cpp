d5e5a674622676520d8d3f524e7315a3da73fc7b
Kimberly Head <sara24rhodes@gmail.com>
2026-02-08 11:27:59 -0600

    Create greetings.cpp
    Practicing printing hello in 3 different languages and using multi-line comments and single line comments

---
+++ greetings.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+/* Program that says 'Hello, World' in 
+   3 languages: English, French, and Spanish */
+int main() {
+    std::cout << "Hello, World (English)\n"; // English translation
+    std::cout << "Bonjour, le monde! (French)\n"; // French translation
+    std::cout << "Hola, Mundo! (Spanish)\n";  // Spanish translation
+    
+    return 0;
+}
