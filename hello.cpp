65c41d9a12cbcb5c1abced27aedeef272ebeb802
Kimberly Head <sara24rhodes@gmail.com>
2026-02-07 22:49:05 -0600

    Create hello.cpp
    First C++ program

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ // This is a single-line comment
+ // It explains what the program does
+
+ /* 
+ This is a multi-line comment
+ You can write multiple lines of explanations
+ Without worrying about adding // to each line
+ */
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
