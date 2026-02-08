edce926b6a4b3a92b751b1c0c963155076082046
Kimberly Head <sara24rhodes@gmail.com>
2026-02-07 22:57:51 -0600

    Create hello2.cpp
    Second C++ program that also shows using \n  instead of endl

---
+++ hello2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+int main() {
+    // Using std::endl for line breaks
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Welcome to C++ Programming!" << std::endl;
+
+    // Using \n for line breaks
+    std::cout << "Using newline character:\n";
+    std::cout << "Line 1\n";
+    std::cout << "Line 2\n";
+
+    // Mixing both methods
+    std::cout << "Mixing methods: " << std::endl;
+    std::cout << "Line with endl\n";
+    std::cout << "Line with newline character" << std::endl;
+
+    return 0;
+}
