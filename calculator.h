aa6ec49f3b57cf1566954bacae739b63d924eb71
Kimberly Head <sara24rhodes@gmail.com>
2026-09-12 14:28:08 -0500

    Add Calculator class with basic operations

---
+++ calculator.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+    // Function prototypes for calculator operations
+    int add(int a, int b);
+    int subtract(int a, int b);
+    int calculate(int a, int b, char operation);
+};
+
+#endif // CALCULATOR_H
