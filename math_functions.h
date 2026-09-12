2bf59714dc9953e1a32d3700e5a83c70d984dabf
Kimberly Head <sara24rhodes@gmail.com>
2026-09-12 14:25:38 -0500

    Add header file for mathematical operations

---
+++ math_functions.h
@@ -0,0 +1,10 @@
+#ifndef MATH_FUNCTIONS_H
+#define MATH_FUNCTIONS_H
+
+// Function prototypes for mathematical operations
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+double divide(double a, double b);
+
+#endif // MATH_FUNCTIONS_H
